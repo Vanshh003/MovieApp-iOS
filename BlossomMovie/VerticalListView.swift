88ea6ce677c5777f18daa96a35871024bde48c58
Vansh Aggarwal <106269430+Vanshh003@users.noreply.github.com>
2026-03-30 02:08:31 +0530

    Update VerticalListView.swift

--- BlossomMovie/VerticalListView.swift
+++ BlossomMovie/VerticalListView.swift
@@ -5,18 +5,23 @@
 //  Created by Vansh Aggarwal on 13/03/26.
 //
 
+
+
+// used by both upcomingView and downloadView. each row shows a poster thumbnail on the left and the title name on the right
+
+
 import SwiftUI
 import SwiftData
 
 struct VerticalListView: View {
     var titles: [Title]
-    let canDelete: Bool
+    let canDelete: Bool     // controls whether swipe to depete is enabled. 
     
     @Environment(\.modelContext) var modelContext
     
     var body: some View {
         List(titles) { title in
-            NavigationLink {
+            NavigationLink {            // NavigationLink wraps each row so tapping tit navigates to TitleDetailView
                 TitleDetailView(title: title)
             } label: {
                 AsyncImage(url: URL(string: title.posterPath ?? "")) { image in
