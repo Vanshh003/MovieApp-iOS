aaed09d5eefc5279b51ab4136857963143f36a14
Vansh Aggarwal <106269430+Vanshh003@users.noreply.github.com>
2026-03-30 02:07:30 +0530

    Update SearchViewModel.swift

--- BlossomMovie/SearchViewModel.swift
+++ BlossomMovie/SearchViewModel.swift
@@ -5,6 +5,9 @@
 //  Created by Vansh Aggarwal on 26/03/26.
 //
 
+
+// search data manager
+
 import Foundation
 
 @Observable
@@ -13,6 +16,9 @@ class SearchViewModel {
     private(set) var searchTitles: [Title] = []
     private let dataFetcher = DataFetcher()
     
+    // if the search text is empty, it loads trending titles as a default.
+    // if there is text, it runs a real search
+    // this gives the search screen content to show even before the user types anything
     func getSearchTitles(by media: String, for title: String) async {
         do {
             errorMessage = nil
@@ -27,3 +33,6 @@ class SearchViewModel {
         }
     }
 }
+
+
+// 'private(set)' -> only this class can write these values, but anyone can read them. this prevents other files from accidentally changind the search results
