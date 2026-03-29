5b3726def0c38c861b03cd008c456e276d736017
Vansh Aggarwal <106269430+Vanshh003@users.noreply.github.com>
2026-03-30 02:05:01 +0530

    Update BlossomMovieApp.swift

--- BlossomMovie/BlossomMovieApp.swift
+++ BlossomMovie/BlossomMovieApp.swift
@@ -12,8 +12,18 @@ import SwiftData
 struct BlossomMovieApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView()       // first screen shown
         }
-        .modelContainer(for: Title.self)
+        .modelContainer(for: Title.self)    // set up a local database that knows how to store 'Title' objects
     }
 }
+
+// WindowGroup -> window of iphone screen
+
+
+// .modelContainer(for: Title.self)
+//        - It Creates the Physical Storage (SwiftData uses a database called SQLite under the hood. When this line runs, the app checks if a database              file already exists on the user's phone.)
+//
+//        -  It Defines the "Schema" (The Blueprint) By passing Title.self, you are telling SwiftData:
+//            "I want to save objects of type Title. Look at the Title class and create a table with columns that match its properties
+
